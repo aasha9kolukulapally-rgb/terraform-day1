0375efdb6c69e2c03ede14e378e0272366112ede
--- <--->
2025-10-30 16:27:20 +0530

    add

---
+++ day-4-statelocking/backend.tf
@@ -0,0 +1,10 @@
+
+terraform {
+  backend "s3" {
+    bucket = "chinnarikutty"
+     key    = "terraform.tfstate"
+    use_lockfile = true # to use s3 native locking 1.19 version above
+    region = "us-west-1"
+  }
+}
+
