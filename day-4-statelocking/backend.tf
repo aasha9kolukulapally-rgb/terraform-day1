be27dd8a72568463a23716d98da637d82b36efa9
--- <--->
2025-10-31 20:30:23 +0530

    day-custom-nw

--- day-4-statelocking/backend.tf
+++ day-4-statelocking/backend.tf
@@ -3,8 +3,11 @@ terraform {
   backend "s3" {
     bucket = "chinnarikutty"
      key    = "terraform.tfstate"
-    use_lockfile = true # to use s3 native locking 1.19 version above
+    #use_lockfile = true # to use s3 native locking 1.19 version above
     region = "us-west-1"
+   dynamodb_table = "statelock" # any version we can use dynamodb locking 
+    encrypt = true
   }
 }
+
 
