946bffed155adac7bbdb2fbe56184d9c2dd45093
aasha9kolukulapally-rgb <aasha9kolukulapally@gmail.com>
2025-11-19 17:43:40 +0530

    Update main.tf

--- Day-3-statefile/main.tf
+++ Day-3-statefile/main.tf
@@ -1,17 +1,27 @@
-resource "aws_instance" "name" { 
-    instance_type = var.type
-     ami = var.ami_id
-     tags = {
-       Name = "aasha"
-     }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 
-
-  
+  backend "s3" {
+    bucket = "your-tfstate-bucket"
+    key    = "day3/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
-#resource "aws_s3_bucket" "name" {
-  #bucket = "chinnarikutty"
-  
-#}
 
+provider "aws" {
+  region = var.region
+}
 
-  
+resource "aws_instance" "name" {
+  instance_type = var.type
+  ami           = var.ami_id
+
+  tags = {
+    Name = "aasha"
+  }
+}
