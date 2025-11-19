2b22dfc76909f34c057c490fb0cb840594bbdc16
aasha9kolukulapally-rgb <aasha9kolukulapally@gmail.com>
2025-11-19 17:42:37 +0530

    Update variable.tf

--- Day-3-statefile/variable.tf
+++ Day-3-statefile/variable.tf
@@ -1,11 +1,14 @@
 variable "ami_id" {
   description = "Passing AMI values"
-  default     = ""
   type        = string
 }
 
 variable "type" {
   description = "Passing values to instance type"
-  default     = ""
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
   type        = string
 }
